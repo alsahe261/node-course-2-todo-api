3ab3814d77c21bc5c54ebecd370eb22ff61ca1e8
Alexander Salguero <alsahe26@gmail.com>
2018-05-13 00:52:37 -0500

    POST

--- server/server.js
+++ server/server.js
@@ -1,20 +1,31 @@
-var mongoose = require('mongoose')
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+      res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
 
 // Save new something
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
-  }
-});
 
 /*var newTodo = new Todo({
   text: 'Cook dinner'
@@ -26,14 +37,30 @@ newTodo.save().then((doc) => {
     console.log('Unable to save todo');
 });*/
 
-var otherTodo = new Todo({
+/*var otherTodo = new Todo({
   text: 'Feed the cat',
   completed: true,
   completedAt: 123
+});*/
+
+/*var otherTodo = new Todo({
+  text: '   Edit the video2  ',
 });
 
 otherTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2););
+  console.log(JSON.stringify(doc, undefined, 2));
+  mongoose.disconnect();
 }, (e) => {
     console.log('Unable to save todo',e);
 });
+
+var user1 = new User({
+  email: "alsahe26@hotmail.com"
+});
+
+user1.save().then((doc) => {
+  console.log(JSON.stringify(doc, undefined, 2));
+  mongoose.disconnect();
+}, (e) => {
+    console.log('Unable to save todo',e);
+}); */
