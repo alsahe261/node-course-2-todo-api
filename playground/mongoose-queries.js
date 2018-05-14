94ed89e0c2155636c5e28809f44392e732e513f6
Alexander Salguero <alsahe26@gmail.com>
2018-05-14 15:15:05 -0500

    Mongoose Queries PlayGround

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,9 +1,25 @@
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
 
-var id = '5af92064ba30241906540654';
+const {ObjectID} = require('mongodb');
 
-Todo.find({
+//var id = '&5af92064ba30241906540654';
+var id = '5af7c3d6514c321a06007928';
+
+if (!ObjectID.isValid(id)){
+  console.log('ID not valid');
+  return;
+}
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('Id not found');
+  }
+  console.log('user by Id', user);
+}).catch((e) => console.log(e));
+
+/*Todo.find({
   _id: id
 }).then((todos) => {
   console.log('Todos', todos);
@@ -13,11 +29,11 @@ Todo.findOne({
   _id: id
 }).then((todos) => {
   console.log('Todos', todos);
-});
+});*/
 
-Todo.findById(id).then((todo) => {
+/*Todo.findById(id).then((todo) => {
   if (!todo) {
     return console.log('Id not found');
   }
   console.log('Todo by Id', todo);
-});
+}).catch((e) => console.log(e));*/
